ff1dfbf81494f877487a71855de931a580a90661
banandza <116403556+banandza@users.noreply.github.com>
2024-01-19 09:34:32 +0300

    clean

--- Client/Data/DisciplineDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Client.Data
-{
-    public class DisciplineDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int Hours { get; set; } = 0;
-    }
-}
